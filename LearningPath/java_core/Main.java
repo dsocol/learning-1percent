e6612deeed9955d9e4c7aa6a1636a9f7c42dcc74
dsocol <10908097+dsocol@users.noreply.github.com>
2026-02-22 11:13:16 +0200

    Lectia 1: Variabile si configurare .gitignore

---
+++ LearningPath/java_core/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tipuri primitive (simple)
+        int varsta = 25; 
+        double pret = 19.99;
+        boolean esteProgramator = true;
+
+        // Tipuri Obiect (complexe)
+        String nume = "Daniel"; 
+
+        System.out.println(nume + " are " + varsta + " ani.");
+    }
+}
